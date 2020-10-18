f18fcb17bc245c3cf8534d34177ab4b357146b39
Alberto Sánchez <albertoensan@gmail.com>
2020-10-18 18:50:08 +0200

    Add new Spinner.js compounent with defaultsProps

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay.js';
+import Spinner from './Spinner.js';
 
 class App extends React.Component{
     constructor(props) { 
@@ -28,7 +29,7 @@ class App extends React.Component{
             );    
         }
 
-        return (<div>Loading!</div>);
+        return <Spinner message="Please accept location request"></Spinner>;
     }
 }
 
